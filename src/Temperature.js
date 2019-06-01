08012ef9308f5fbde5d87673078badae8d232479
Nagindra <nagi@Nagindras-MacBook-Air.local>
2019-06-01 19:57:17 +0200

    built temperature component

---
+++ src/Temperature.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './temp.css';
+
+const Temp = (props) => {
+    const kelvinConversion = 273.15;
+    const roundtemp = Math.round((props.temp - kelvinConversion), 2);
+    const iconUrl = 'http://openweathermap.org/img/w/' + props.icon +'.png';
+    
+    return ( 
+        <div className="temp">
+            <img className="img" src={iconUrl} alt=""></img>
+            <span>{roundtemp}</span>
+        </div>   
+    );
+} 
+export default Temp;
